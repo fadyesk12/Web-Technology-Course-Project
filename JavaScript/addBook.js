984046c7cc30cf200845ba00f6731503c92e4e7c
fadyesk12 <136813453+fadyesk12@users.noreply.github.com>
2024-04-20 13:08:53 +0200

    Added functionality to add book info to local storage

---
+++ JavaScript/addBook.js
@@ -0,0 +1,52 @@
+// Preview the selected image before submitting the form
+const coverImageInput = document.getElementById('coverImage');
+const imagePreview = document.getElementById('imagePreview');
+
+var coverImage;
+
+coverImageInput.addEventListener('change', function () {
+  const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function () {
+      const img = new Image();
+      img.src = reader.result;
+      img.className = 'preview';
+      imagePreview.innerHTML = '';
+      imagePreview.appendChild(img);
+      // saving the image as a base 64 encoded string
+      coverImage = document.getElementById('imagePreview').firstChild;
+      imgData = coverImage.src;
+      localStorage.setItem("imgData", imgData);
+    }
+    reader.readAsDataURL(file);
+  } else {
+    imagePreview.innerHTML = '';
+  }
+});
+
+//Store book info in local storage
+document.getElementById('book-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var book = {
+        bookName: document.getElementById('book-name').value,
+        bookId: document.getElementById('book-id').value,
+        author: document.getElementById('author').value,
+        category: document.getElementById('category').value,
+        description: document.getElementById('description').value,
+        imgData: ''
+    };
+
+    var bookList = JSON.parse(localStorage.getItem("books"))  || [];
+    var image = localStorage.getItem("imgData");
+    var ID = book.bookId;
+    var bookCoverKey = "Cover" + ID;
+    localStorage.setItem(bookCoverKey, image);
+    book.imgData = bookCoverKey;
+    bookList.push(book);
+    bookList = JSON.stringify(bookList);
+    localStorage.setItem("books", bookList);
+    localStorage.removeItem("imgData")
+    // window.location.href = "";
+});
