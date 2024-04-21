4359a18669ce78477c12bd83703bf8ee097a8e67
fadyesk12 <136813453+fadyesk12@users.noreply.github.com>
2024-04-21 15:04:27 +0200

    borrow book page dynamically updates borrow list and chooses book based on query

--- JavaScript/borrow.js
+++ JavaScript/borrow.js
@@ -3,3 +3,35 @@ document.getElementById("searchForm").addEventListener("submit", function(event)
     var searchType = "T";
     window.location.href = "./book_list.html?" + document.getElementById("searchText").value + searchType;
 })
+
+var list = JSON.parse(localStorage.getItem("books"))  || [];
+var selectMenu = document.getElementById("book");
+
+list.forEach(book => {
+    if(book.borrowStatus == "0"){
+        var option = document.createElement("option");
+        option.value = book.bookName;
+        option.innerHTML = book.bookName;
+        selectMenu.appendChild(option)
+    }
+})
+
+var query = window.location.search;
+query = query.substring(1);
+query = query.replace(/%20/g, " ");
+console.log(query);
+if(query){
+    for(let i = 0; i < selectMenu.options.length; i++){
+        if(selectMenu.options[i].text == query){
+            console.log("true");
+            selectMenu.selectedIndex = i;
+            break;
+        }
+    }
+}
+
+var form = document.getElementById("borrowForm");
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+})
