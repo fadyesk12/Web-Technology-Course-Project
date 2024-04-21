a57840c8dbd87a620ff9d0aea6017b23f9bf3d44
fadyesk12 <136813453+fadyesk12@users.noreply.github.com>
2024-04-21 21:54:11 +0200

    removed comments related to cookies

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -46,29 +46,6 @@ else{
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    // // Function to set a cookie
-    // function setCookie(name, value, days) {
-    //     var expires = "";
-    //     if (days) {
-    //         var date = new Date();
-    //         date.setTime(date.getTime() + (days*24*60*60*1000));
-    //         expires = "; expires=" + date.toUTCString();
-    //     }
-    //     document.cookie = name + "=" + (value || "") + expires + "; path=/";
-    // }
-
-    // // Function to get a cookie
-    // function getCookie(name) {
-    //     var nameEQ = name + "=";
-    //     var ca = document.cookie.split(';');
-    //     for(var i=0; i < ca.length; i++) {
-    //         var c = ca[i];
-    //         while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    //         if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    //     }
-    //     return null;
-    // }
-
     // Attach an event listener for the form submission
     document.getElementById('userForm').addEventListener('submit', function(event) {
         event.preventDefault(); // Prevent the form from submitting normally
