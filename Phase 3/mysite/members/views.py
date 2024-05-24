c8508aaafc29bdb6ed4d611ca1113c0fcce4f7ef
fadyesk12 <136813453+fadyesk12@users.noreply.github.com>
2024-05-24 14:33:00 +0300

    ajax

---
+++ Phase 3/mysite/members/views.py
@@ -0,0 +1,134 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.template import loader
+from .models import Users, Books
+from django.views.decorators.csrf import csrf_exempt
+# Create your views here.
+
+@csrf_exempt
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+@csrf_exempt
+def Add_book(request):
+    template = loader.get_template('Add_book.html')
+    return HttpResponse(template.render())
+
+@csrf_exempt
+def admin(request):
+    template = loader.get_template('admin.html')
+    return HttpResponse(template.render())
+
+@csrf_exempt
+def book_list(request):
+    template = loader.get_template('book_list.html')
+    return HttpResponse(template.render())
+
+@csrf_exempt
+def bookDisplay(request):
+    template = loader.get_template('bookDisplay.html')
+    return HttpResponse(template.render())
+
+@csrf_exempt
+def borrow(request):
+    template = loader.get_template('borrow.html')
+    return HttpResponse(template.render())
+
+@csrf_exempt
+def dashboard(request):
+    template = loader.get_template('dashboard.html')
+    return HttpResponse(template.render())
+
+@csrf_exempt
+def edit(request):
+    template = loader.get_template('edit.html')
+    return HttpResponse(template.render())
+
+@csrf_exempt
+def login(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
+
+@csrf_exempt
+def signup(request):
+    template = loader.get_template('signup.html')
+    return HttpResponse(template.render())
+
+@csrf_exempt
+def addUser(request):
+    data = {'success': False} 
+    if request.method == 'POST':
+        uID = request.POST.get('userID')
+        Name = request.POST.get('userName')
+        Type = request.POST.get('userType')
+        Password = request.POST.get('userPassword')
+        user = Users()
+        user.userID = uID
+        user.userName = Name
+        user.userType = Type
+        user.userPassword = Password
+        user.save()
+    data['success'] = True
+    return JsonResponse(data)
+
+@csrf_exempt
+def addBook(request):
+    data = {'success': False} 
+    if request.method == 'POST':
+        Name = request.POST.get('bookName')
+        bID = request.POST.get('bookID')
+        auth = request.POST.get('author')
+        cat = request.POST.get('category')
+        desc = request.POST.get('description')
+        status = request.POST.get('borrowStatus')
+        img = request.POST.get('imgData')
+        book = Books()
+        book.bookName = Name
+        book.bookID = bID
+        book.author = auth
+        book.category = cat
+        book.description = desc
+        book.borrowStatus = status
+        book.imgData = img
+        book.save()
+    data['success'] = True
+    return JsonResponse(data)
+
+@csrf_exempt
+def checkUser(request):
+    obj = list(Users.objects.filter(userName = request.POST.get('Username')).values())
+    for x in obj:
+        if x['userPassword'] == request.POST.get('Password'):
+            data = {'data' : obj}
+            return JsonResponse(data)
+    data = {'success': False} 
+    return JsonResponse(data)
+
+@csrf_exempt
+def retrieveBooks(request):
+    obj = list(Books.objects.all().values())
+    data = {'list': obj}
+    return JsonResponse(data)
+
+@csrf_exempt
+def deleteBook(request):
+    Books.objects.filter(bookID=request.POST.get('bID')).delete()
+    data = {'success': True} 
+    return JsonResponse(data)
+
+@csrf_exempt
+def updateUser(request):
+    Users.objects.filter(userID = request.POST.get('uID')).update(borrowedBooks = request.POST.get('borrowedList'))
+    
+@csrf_exempt
+def retrieveBorrowed(request):
+    obj = list(Users.objects.filter(userID = request.POST.get('uID')).values())
+    data = {'list': obj}
+    return JsonResponse(data)
+
+
+@csrf_exempt
+def borrowBook(request):
+    Books.objects.filter(bookID = request.POST.get('bID')).update(borrowStatus = request.POST.get('status'))
---
+++ Phase 3/mysite/static/JavaScript/login.js
@@ -0,0 +1,127 @@
+//Determine the user type and what they're supposed to have access to in the navigation bar
+var navbar = document.getElementById("navbar");
+var userList = JSON.parse(localStorage.getItem("userInfoArray")) || [];
+var cookieID = localStorage.getItem("userID");
+var loginType;
+if(userList){
+    userList.forEach(user => {
+    if(user.data[0].userID == cookieID){
+        loginType = user.data[0].userType;
+    }
+    })
+}
+if(loginType == "admin"){
+    var borrowButton = document.createElement("a");
+    borrowButton.href = "./borrow.html";
+    borrowButton.innerHTML = "Borrow";
+    var dashboardButton = document.createElement("a");
+    dashboardButton.href = "./dashboard.html";
+    dashboardButton.innerHTML = "Dashboard";
+    var adminButton = document.createElement("a");
+    adminButton.href = "./admin.html";
+    adminButton.innerHTML = "Admin";
+    navbar.appendChild(borrowButton);
+    navbar.appendChild(dashboardButton);
+    navbar.appendChild(adminButton);
+}
+else if(loginType == "user"){
+    var borrowButton = document.createElement("a");
+    borrowButton.href = "./borrow.html";
+    borrowButton.innerHTML = "Borrow";
+    var dashboardButton = document.createElement("a");
+    dashboardButton.href = "./dashboard.html";
+    dashboardButton.innerHTML = "Dashboard";
+    navbar.appendChild(borrowButton);
+    navbar.appendChild(dashboardButton);
+}
+else{
+    var loginButton = document.createElement("a");
+    loginButton.href = "./login.html";
+    loginButton.innerHTML = "Login";
+    var signupButton = document.createElement("a");
+    signupButton.href = "./signup.html";
+    signupButton.innerHTML = "Signup";
+    navbar.appendChild(loginButton);
+    navbar.appendChild(signupButton);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // // Function to set a cookie
+    // function setCookie(name, value, days) {
+    //     var expires = "";
+    //     if (days) {
+    //         var date = new Date();
+    //         date.setTime(date.getTime() + (days*24*60*60*1000));
+    //         expires = "; expires=" + date.toUTCString();
+    //     }
+    //     document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    // }
+
+    // // Function to get a cookie
+    // function getCookie(name) {
+    //     var nameEQ = name + "=";
+    //     var ca = document.cookie.split(';');
+    //     for(var i=0; i < ca.length; i++) {
+    //         var c = ca[i];
+    //         while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    //         if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    //     }
+    //     return null;
+    // }
+
+    // Attach an event listener for the form submission
+    document.getElementById('userForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the form from submitting normally
+
+        var loginUsername = document.getElementById('username').value;
+        var loginPassword = document.getElementById('password').value;
+        var user
+        $.ajax({
+            url: "http://127.0.0.1:8000/members/checkUser",
+            type: "POST",
+            data:{
+                Username: loginUsername,
+                Password: loginPassword,
+            },
+            dataType: 'json',
+            success: function (data) {
+                if (data['success'] == false){
+                    alert("check username and password")
+                    return
+                }
+                else{
+                    user = data
+                    var storedUserInfoArray;
+                    try {
+                        storedUserInfoArray = JSON.parse(localStorage.getItem('userInfoArray')) || [];
+                    } catch (error) {
+                        console.error('Error parsing user information from local storage:', error);
+                        alert('An error occurred. Please try again.');
+                        return;
+                    }
+                    storedUserInfoArray.push(user)
+                    console.log(storedUserInfoArray)
+                    var isLoginSuccessful = false;
+                    storedUserInfoArray.forEach(userInfo => {
+                        if (userInfo.data[0].userName == loginUsername) {
+                            isLoginSuccessful = true;
+                            alert('Login successful. User type: ' + userInfo.data[0].userType);
+                            localStorage.setItem("userID", userInfo.data[0].userID);
+                            localStorage.setItem('userInfoArray', JSON.stringify(storedUserInfoArray))
+                            // document.cookie = "userID=" + userInfo.userID + "; path=../";
+                            // console.log(decodeURIComponent(document.cookie));
+                            window.location.href = './index.html';
+                            return; // Exit the loop once a match is found
+                        }
+                    });
+
+                    if (!isLoginSuccessful) {
+                        alert('Login failed. Please check your username and password.');
+                    }
+                }
+            }
+        });
+        
+        
+    });
+});
